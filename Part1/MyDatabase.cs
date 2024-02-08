85b3b2988b5e8d74e5c3846f34e1d715a936a6af
Lisa-cai <145958405+Lisa-cai@users.noreply.github.com>
2024-02-08 00:56:30 -0800

    add code

---
+++ Part1/MyDatabase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_OOP_Midterm_Part1
+{
+    public static class MyDatabase
+    {
+        //static field
+        private static List<Product> data;
+
+        static MyDatabase()
+        {
+            Console.WriteLine("The list is loading");
+            Clothing tShirt = new Clothing("T-shirt", 19.99, "Medium", "Red");
+            Electronics smartphone = new Electronics("Smartphone", 799.99, "Apple");
+            Groceries banana = new Groceries("Banana", 0.99, "2024-02-10");
+
+            data.Add(tShirt);
+            data.Add(smartphone);
+            data.Add(banana);
+
+        }
+        // Static property
+        public static List<Product> Data 
+        {
+            get { return data; }
+        }
+
+
+
+    }
+}
